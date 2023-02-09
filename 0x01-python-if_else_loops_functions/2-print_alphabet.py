4df5c3a6410ed72e97db7157c2f39391cf04e561
adeolu ogunleye <ogunleyeea@gmail.com>
2023-02-09 02:45:33 -0800

    2-print_alphabet.p

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 
-def add(a, b):
-    return a + b
+def print_low_alpha():
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        print('{}'.format(c), end='')
 
 
 if __name__ == '__main__':
-    print(add(1, 2))
+    print_low_alpha()
