2e872e663f734770cdb5b54d04bd2c7fc1bc33b8
adeolu ogunleye <ogunleyeea@gmail.com>
2023-02-14 04:22:44 -0800

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,4 +9,4 @@ def print_reversed_list_integer(my_list=[]):
 
 if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5]
-    print_reversed_list_integer(my_list
+    print_reversed_list_integer(my_list)
