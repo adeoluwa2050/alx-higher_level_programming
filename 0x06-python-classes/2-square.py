8cc283ab18c9c6d0bd533055752d30f4bf3f7e5c
adeolu ogunleye <ogunleyeea@gmail.com>
2023-02-20 03:53:04 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,4 +15,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
+        self.__size = size
