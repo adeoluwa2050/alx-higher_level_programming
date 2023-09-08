0afc3d45fc09b4cc98dfabf6274ef8cb74054c8e
adeoluwa2050 <ogunleyeea@gmail.com>
2023-09-09 00:23:03 +0200

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module to read and print file contents
+"""
+
+
+def read_file(filename="")
+    """Reads file and prints contents
+    """
+    with open(filename) as open_file:
+        content = open_file.read()
+    print(contents, end="")
