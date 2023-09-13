0a3e883091555d00e9bba85ab989d08d6645ec42
adeoluwa2050 <ogunleyeea@gmail.com>
2023-09-13 21:58:29 +0200

    Input Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-Module to read and print file contents
-"""
+""" This is a module to read and print file contents """
 
-
-def read_file(filename="")
-    """To read file and print contents
-    """
+def read_file(filename=""):
+    """ To read file and print contents """
     with open(filename) as open_file:
-        content = open_file.read()
+        contents = open_file.read()
     print(contents, end="")
